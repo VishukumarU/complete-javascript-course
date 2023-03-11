856a0ef54eeb00ca7e9da598a3ab380bced406ba
Vishu Kumar <1992vishukumar@gmail.com>
2023-03-11 15:59:53 +0530

    232: Using the GeoLocation API

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,21 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+/* 
+    232: Using the GeoLocation API
+*/
+
+navigator.geolocation.getCurrentPosition(
+    (position) => {
+        console.log(position);
+        const {latitude, longitude} = position.coords;
+        console.log(`https://www.google.com/maps/@${latitude},${longitude},15z`);
+    },
+    (error) => {
+        console.log(error);
+    }, {
+    // enableHighAccuracy: true,
+    // maximumAge: 2,
+    // timeout: 1
+})
