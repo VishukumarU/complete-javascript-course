11498c2e4cc231f2f02c6dd944c078052b2ec7ac
Vishu Kumar <1992vishukumar@gmail.com>
2023-03-14 09:07:13 +0530

    237: Refactoring project architecture

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,44 +11,93 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-/* 
-    232: Using the GeoLocation API
-*/
 
-navigator.geolocation.getCurrentPosition(
-    (position) => {
-        console.log(position);
+class App {
+
+    #map;
+    #mapEvent;
+
+    constructor () {
+        this._addEventListeners();
+        this._getPosition();
+    }
+
+    _addEventListeners () {
+        // This keyword will point to the object on which we are attaching the eventlistener
+        // So, bind the class
+        form.addEventListener('submit', this._newWorkout.bind(this));
+        inputType.addEventListener('change', this._toggleElevationFields)
+    }
+
+    _getPosition () {
+        /* 
+            232: Using the GeoLocation API
+        */
+
+        navigator.geolocation && navigator.geolocation.getCurrentPosition(
+            this._loadMap.bind(this),
+            (error) => alert(`Couldn't get the co-ordinates`),
+            {});
+    }
+
+    _loadMap (position) {
         const {latitude, longitude} = position.coords;
-        console.log(`https://www.google.com/maps/@${latitude},${longitude},15z`);
 
         /* 
             233: Displaying a Map using leaflet library
         */
-        const coords = [latitude, longitude];
-        const map = L.map('map').setView(coords, 15);
+
+        this.#map = L.map('map').setView([latitude, longitude], 15);
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
-        }).addTo(map);
+        }).addTo(this.#map);
+        this.#map.on('click', this._showForm.bind(this));
+    }
+
+    /* 
+        235: Rendering input workout form
+    */
+    _showForm (event) {
+        this.#mapEvent = event;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+    }
+
+    _toggleElevationField () {
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    }
+
+    _newWorkout (e) {
+        e.preventDefault();
+        this._clearInputFields();
 
         /* 
-            234: Displaying a map marker
+           234: Displaying a map marker
         */
+        const {lat, lng} = this.#mapEvent.latlng;
+        L.marker([lat, lng])
+            .addTo(this.#map)
+            .bindPopup(L.popup({
+                maxWidth: 200,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup'
+            }))
+            .setPopupContent('Workout!!')
+            .openPopup();
+    }
 
-        map.on('click', e => {
-            const {lat, lng} = e.latlng;
-            L.marker([lat, lng]).addTo(map)
-                .bindPopup(L.popup({
-                    maxWidth: 200,
-                    minWidth: 100,
-                    autoClose: false,
-                    closeOnClick: false,
-                    className: 'running-popup'
-                }))
-                .setPopupContent('Workout!!')
-                .openPopup();
-        })
-    },
-    (error) => {
-        console.log(error);
-    }, {});
+    _clearInputFields () {
+        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+    }
+}
+
+const app = new App();
+
+
+
+
+
