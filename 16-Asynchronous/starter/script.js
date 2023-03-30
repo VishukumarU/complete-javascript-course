1b69169bcc19085699c76ae391b4a59fd9c41cf6
Vishu Kumar <1992vishukumar@gmail.com>
2023-03-30 08:51:10 +0530

    256: Coding Challenge #1

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -90,33 +90,33 @@ const renderCountry = (data, className = '') => {
 //     }, 1000);
 // }, 1000);
 
-/* 
+/*
     251: Promises and the fetch API
 */
 
-const request = fetch(`${baseUrl}/name/india?fullText=true`);
-console.log(request);
+// const request = fetch(`${baseUrl}/name/india?fullText=true`);
+// console.log(request);
 
-/* 
-    252: Consuming promises
-*/
+// /* 
+//     252: Consuming promises
+// */
 
 const renderError = (msg) =>
     countriesContainer.insertAdjacentText('afterend', msg);
 
-const getJSON = (url, errorMessage = 'Something went wrong!') => {
-    return fetch(url)
-        .then((response) => {
+// const getJSON = (url, errorMessage = 'Something went wrong!') => {
+//     return fetch(url)
+//         .then((response) => {
 
-            /* 
-                255:Throwing errors manually
-            */
-            if (!response.ok) {
-                throw new Error(`${errorMessage} (${response.status})`);
-            }
-            return response.json();
-        });
-};
+//             /* 
+//                 255:Throwing errors manually
+//             */
+//             if (!response.ok) {
+//                 throw new Error(`${errorMessage} (${response.status})`);
+//             }
+//             return response.json();
+//         });
+// };
 
 const getCountryData = (country) => {
     getJSON(`${baseUrl}/name/${country}?fullText=true`, 'Country not found')
@@ -141,11 +141,84 @@ const getCountryData = (country) => {
         })
 }
 
-btn.addEventListener('click', () => {
-    // getCountryData('sdhjfj');
-    getCountryData('germany');
-    // getCountryData('Australia');
+// btn.addEventListener('click', () => {
+//     // getCountryData('sdhjfj');
+//     getCountryData('germany');
+//     // getCountryData('Australia');
 
-});
+// });
 
 // getCountryData('germany');
+
+/* 
+    256: Coding Challenge #1
+*/
+
+/* 
+    In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. 
+    For that, you will use a second API to geocode coordinates.
+
+    Here are your tasks:
+
+    PART 1
+    1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) 
+        (these are GPS coordinates, examples are below).
+    2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, 
+        like a city and country name. Use this API to do reverse geocoding: https://geocode.xyz/api.
+        The AJAX call will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json.
+        Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
+    3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location.
+        Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
+    4. Chain a .catch method to the end of the promise chain and log errors to the console
+    5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403.
+        This is an error with the request. Remember, fetch() does NOT reject the promise in this case.
+        So create an error to reject the promise yourself, with a meaningful error message.
+
+    PART 2
+    6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, 
+        and plug it into the countries API that we have been using.
+    7. Render the country and catch any errors, just like we have done in the last lecture 
+        (you can even copy this code, no need to type the same code)
+
+    TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+    TEST COORDINATES 2: 19.037, 72.873
+    TEST COORDINATES 2: -33.933, 18.474
+
+    GOOD LUCK 😀
+*/
+
+const getJSON = (url, errorMsg = 'Something went wrong!!') => {
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                if (response.status === 403) {
+                    throw new Error('Exceeded the limit of up to 1 per second.');
+                }
+
+                throw new Error(`${errorMsg} (${response.status})`)
+            }
+            return response.json();
+        })
+};
+
+const whereAmI = (lat, lng) => {
+    console.log(lat, lng);
+    getJSON(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+        .then((response) => {
+            if (response.error) {
+                throw new Error('Coords are not valid');
+            }
+            console.log(`(${lat}, ${lng}) -- You are in ${response.city}, ${response.country}`);
+            return response.country;
+        })
+        .then(country => getCountryData(country))
+        .catch(err => {
+            console.error(`${err.message} Try again!!`);
+        })
+
+};
+
+// whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
+// whereAmI(-33.933, -1200.474);
