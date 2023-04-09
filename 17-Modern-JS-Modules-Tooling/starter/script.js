707cddf4ada8f3d908b36d3562d80a48e89dcc9a
Vishu Kumar <1992vishukumar@gmail.com>
2023-04-09 18:40:42 +0530

    274: The module pattern

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -16,38 +16,68 @@ console.log('Importing module');
 // default import
 import add, { cart } from './clean.js';       // not a good practice to mix the styles of imports. Done here for demo
 
-add('pizza', 2);
-add('chapati', 5);
-add('dose', 6);
+// add('pizza', 2);
+// add('chapati', 5);
+// add('dose', 6);
 
 // // variables in the import point to the same place in memory as in the exported file
 // console.log(cart);
 
-/* 
+/*
     273: Top-level await(ES2022)
 */
 
-// console.log('start fetching');
+// // console.log('start fetching');
 
-// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-// const data = await res.json();
-// console.log(data);
+// // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// // const data = await res.json();
+// // console.log(data);
 
-// console.log('fetch complete');
+// // console.log('fetch complete');
 
-const getLastPost = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const data = await res.json();
-    console.log(data);
-    return { title: data.at(-1).title, text: data.at(-1).body }
-};
+// const getLastPost = async () => {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//     const data = await res.json();
+//     console.log(data);
+//     return { title: data.at(-1).title, text: data.at(-1).body }
+// };
 
-const lastPost = getLastPost();
-console.log(lastPost);  // async function will return a promise
+// const lastPost = getLastPost();
+// console.log(lastPost);  // async function will return a promise
 
-// Not clean
-lastPost.then((post) => console.log(post));
+// // Not clean
+// lastPost.then((post) => console.log(post));
 
-// cleaner with top level await
-const lastPost2 = await getLastPost();
-console.log(lastPost2);
+// // cleaner with top level await
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2);
+
+/* 
+    274: The module pattern
+*/
+
+const ShippingCartModule = (() => {
+    const shippingCost = 10;
+    const cart = [];
+    const totalPrice = 237;
+    const totalQuantity = 23;
+
+    const addToCart = (product, quantity) => {
+        cart.push({ product, quantity });
+        console.log(`${ quantity } ${ product } added to the cart(shipping cost is ${ shippingCost })`);
+    };
+
+    const order = (product, quantity) => {
+        console.log(`${ quantity } ${ product } ordered`);
+    };
+
+    return {
+        totalPrice,
+        totalQuantity,
+        addToCart,
+        cart
+    };
+})();
+
+ShippingCartModule.addToCart('dose', 5);
+ShippingCartModule.addToCart('pizza', 2);
